4e18353d1ff1a06d67adaebe023c1fa4c1dd8ade
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 23:41:21 +0530

    using fs module user-data.txt file is created.

--- modulesAndFileSystem.js
+++ modulesAndFileSystem.js
@@ -1,2 +1,10 @@
 // this will pulls-in File System Module in this js file. This fs module is already installed with node.js server.
 const fs = require("fs");
+
+fs.writeFile("user-data.txt", "userName = Sandeep Dalal", err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("wrote to file!");
+  }
+});
