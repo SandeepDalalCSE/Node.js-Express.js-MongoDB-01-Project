86d4a446bd0633f165820c56951d55b6eb18d6d9
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 23:58:03 +0530

    creating server and listening on port 3000

--- httpRequests.js
+++ httpRequests.js
@@ -1,3 +1,10 @@
 // pulls-in http module in this file. This is module which is required to work with http requests.
 // Note: We can listen to the requests and also we can create requests and send it them to a server.
 const http = require("http");
+// creating a server
+const server = http.createServer((request, response) => {
+  response.write("this is this demo response");
+  response.end();
+});
+// listening on port 3000
+server.listen(3000);
