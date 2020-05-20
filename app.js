f50f864e6c1ae9f2e3ce2c0c3452f81dd8943ebd
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 23:26:19 +0530

    Note: js core features exists in node.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // We need to instal node.js server from node.org
+// this code will run on server which is node server.
 // We can run this app.js in terminal using cmd as 'node app.js'
 console.log("Use node app.js to run this file.");
+
+// Note core js feature exists
+const userName = "Sandeep";
+console.log(`Hello ${userName}`);
