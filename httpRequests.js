a58c48e5969f154409455040ebf3aca6680a4199
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 23:51:24 +0530

    pulls-in 'http' module

---
+++ httpRequests.js
@@ -0,0 +1,3 @@
+// pulls-in http module in this file. This is module which is required to work with http requests.
+// Note: We can listen to the requests and also we can create requests and send it them to a server.
+const http = require("http");
