65714ef077eb0c8004c3b6435bd1ba46ba484aa1
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-21 16:56:45 +0530

    adding a middleware

--- app.js
+++ app.js
@@ -7,4 +7,14 @@ const app = express(); // creating app object as a express function, which of ha
 //  functions which all received the request and all can do something with it and each function can either stop the request and send back a response,
 //  at which point the request will not reach any other function thereafter, any other middleware or a function forwards the request to the next function,
 //  to the next middleware in line. So a middleware is really just a function that gets the request and then can do something with it.
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  next(); // using next(), we are telling this middleware that we are not done till now, so it will go to the next middleware.
+}); // we are registering a middleware using 'use' method.
+
+// Please note here order matters for execution of middleware.
+app.use((req, res, next) => {
+  res.send("<h1>Hello World</h1>");
+});
 app.listen(3000); // this sets the node server behind the scenes on port 3000
