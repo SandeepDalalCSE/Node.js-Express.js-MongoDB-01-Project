ae83e77c4bffc0b2dd32813f309e7950cca7f760
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-21 08:12:44 +0530

    clearing app.js and copying its data to test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// We need to instal node.js server from node.org
+// this code will run on server which is node server. Basically you can say that javascript running out of browser.
+// We can run this app.js in terminal using cmd as 'node app.js'
+console.log("Use node app.js to run this file.");
+
+// Note core js feature exists
+const userName = "Sandeep";
+console.log(`Hello ${userName}`);
